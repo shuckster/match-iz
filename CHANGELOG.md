8c8332a4137021bc5d6d686d27a02cc44e06b59a
Conan Theobald <github@conans.co.uk>
2021-07-23 19:13:07 +0100

    Basic index.d.ts for type hints and examples

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,6 +7,12 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## [Unreleased]
 
+## [1.3.3] - 2021-07-23
+
+### Added
+
+- Basic `index.d.ts` for type hints and examples
+
 ## [1.3.2] - 2021-07-22
 
 ### Fixed
---
+++ index.d.ts
@@ -0,0 +1,66 @@
+export type TPredicate = (value: any) => boolean
+export type TMatchTester = (value: any) => TEvaluator | any
+export type TMatchHandler = (handler: any) => TMatchTester
+export type TEvaluator = {
+  matched: () => boolean
+  value: () => any
+}
+
+declare module 'match-iz' {
+  export function against(...fns: TMatchTester[]): (match: any) => any
+
+  /**
+   * @example
+   * match(valueToTestAgainst)(
+   *   when(valueOrCondition)(result),
+   *   when(valueOrCondition)(result),
+   *   otherwise(fallbackResult)
+   * )
+   * @param value
+   */
+  export function match(value: any): (...fns: TMatchTester[]) => any
+
+  /**
+   * @example
+   * when('foo')(handler)     // is "foo"
+   * // more:
+   * when([42, 'baz'])        // 42 or "baz"
+   * when(inRange(100, 200))  // inclusive
+   * when(/test/)             // RegExp
+   * when(x => x)             // predicate
+   * when({ value: defined }) // structural
+   * @param pattern
+   */
+  export function when(pattern: any | TPredicate): TMatchHandler
+
+  /**
+   * @example
+   * otherwise(fallbackResult)
+   * @param handler
+   */
+  export function otherwise(handler: any): TMatchTester
+
+  export function defined(value: any): boolean
+  export function empty(value: any): boolean
+  export function truthy(value: any): boolean
+  export function falsy(value: any): boolean
+  export function gt(greaterThan: number): TPredicate
+  export function lt(lessThan: number): TPredicate
+  export function gte(greaterThanOrEqualTo: number): TPredicate
+  export function lte(lessThanOrEqualTo: number): TPredicate
+  export function inRange(min: number, max: number): TPredicate
+  export function startsWith(text: string): TPredicate
+  export function endsWith(text: string): TPredicate
+  export function includes(content: any): TPredicate
+
+  /**
+   * @example
+   * const { x, y, z } = spread(defined)
+   * match(vector)(
+   *   when({ x, y, z })(...)
+   * )
+   * @param fn
+   * @returns object
+   */
+  export function spread(fn: TPredicate): object
+}
