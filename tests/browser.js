b062bbe988a50738e91f105d5d15a87a89f5e305
Conan Theobald <github@conans.co.uk>
2022-10-14 02:01:32 +0100

    test: cover all exports

--- tests/browser.js
+++ tests/browser.js
@@ -1,18 +1,38 @@
 /* globals matchiz */
 const fs = require('fs')
 const srcLib = require('../dist/index.js')
+const srcDateLib = require('../dates/index.js')
+const srcDateUtcLib = require('../dates/utc/index.js')
 
 const browserSrc = fs.readFileSync('./dist/match-iz.browser.js', 'utf8')
 eval(browserSrc)
 
-const srcExports = Object.keys(srcLib).sort()
-const browserExports = Object.keys(matchiz).sort()
+const srcExports = Object.keys(srcLib)
+const srcDateExports = Object.keys(srcDateLib)
+const srcDateUtcExports = Object.keys(srcDateUtcLib)
+
+const browserExports = Object.keys(matchiz)
+const browserUtcExports = Object.keys(matchiz.utc)
 
 let returnCode = 0
 
 srcExports.forEach(exp => {
   if (!browserExports.includes(exp)) {
-    console.error(`./src/browser.mjs is missing export: ${exp}`)
+    console.error(`./src/browser.mjs is missing "root" export: ${exp}`)
+    returnCode = 1
+  }
+})
+
+srcDateExports.forEach(exp => {
+  if (!browserExports.includes(exp)) {
+    console.error(`./src/browser.mjs is missing "dates" export: ${exp}`)
+    returnCode = 1
+  }
+})
+
+srcDateUtcExports.forEach(exp => {
+  if (!browserUtcExports.includes(exp)) {
+    console.error(`./src/browser.mjs is missing "dates/utc" export: ${exp}`)
     returnCode = 1
   }
 })
