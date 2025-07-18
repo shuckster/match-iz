7af401f182f75b9d78ce2f4f09de20bcae34d448
Conan Theobald <github@conans.co.uk>
2025-07-18 14:06:21 +0100

    fix: `includedIn` test

--- tests/included-in.test.mjs
+++ tests/included-in.test.mjs
@@ -14,9 +14,9 @@ describe("includedIn()", () => {
   testCases.forEach(({ input, expecting }, index) => {
     test(desc(index, input, expecting), () => {
       const output = match(input)(
-        when(includedIn([1, 2, 3, 0, 4, 5]))("a"),
-        when(includedIn([1, 2, 3, 10, 4, 5]))("b"),
-        when(includedIn([1, 2, 3, 20, 4, 5]))("c"),
+        when(includedIn(1, 2, 3, 0, 4, 5))("a"),
+        when(includedIn(1, 2, 3, 10, 4, 5))("b"),
+        when(includedIn(1, 2, 3, 20, 4, 5))("c"),
       );
 
       assert.strictEqual(output, expecting);
